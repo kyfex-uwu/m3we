511e9c3a71b52a2dd415fa1a291d3536daaf0b80
kyfex-uwu <retrogrape117@gmail.com>
2024-02-15 20:06:24 -0700

    -bumped version -updated lua block texture -fixed lua block, it was broken lol

--- src/main/java/com/kyfexuwu/m3we/luablock/LuaBlockScript.java
+++ src/main/java/com/kyfexuwu/m3we/luablock/LuaBlockScript.java
@@ -1,10 +1,18 @@
 package com.kyfexuwu.m3we.luablock;
 
+import com.kyfexuwu.m3we.Utils;
 import com.kyfexuwu.m3we.lua.CustomScript;
 import com.kyfexuwu.m3we.lua.LuaSurfaceObj;
 import com.kyfexuwu.m3we.lua.ScriptError;
+import com.kyfexuwu.m3we.m3we;
+import net.minecraft.server.world.ServerWorld;
 import org.luaj.vm2.LoadState;
+import org.luaj.vm2.LuaError;
 import org.luaj.vm2.compiler.LuaC;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class LuaBlockScript extends CustomScript {
     public final LuaBlockEntity self;
@@ -17,15 +25,18 @@ public class LuaBlockScript extends CustomScript {
 
     public int revision=0;
     public void updateScript(String script){
-        this.runEnv = CustomScript.safeGlobal();
-        LoadState.install(this.runEnv);
-        LuaC.install(this.runEnv);
         this.revision=(this.revision+1)%Integer.MAX_VALUE;
 
-        var world = this.self.getWorld();
-        if(world!=null&&!world.isClient) {
-            if(!this.isFake) this.runEnv.set("self", new LuaSurfaceObj(this.self));
-            ScriptError.execute(() -> this.runEnv.load(script).call());
-        }
+        this.runEnv = CustomScript.safeGlobal();
+        this.runEnv.set(contextIdentifier, this.contextObj);
+        LoadState.install(this.runEnv);
+        LuaC.install(this.runEnv);
+
+        this.runEnv.set("self", new LuaSurfaceObj(this.self));
+        ScriptError.execute(() -> this.runEnv.load(script).call());
+
+        this.contextObj.javaSet("env", Utils.toLuaValue(this.self.getWorld() instanceof ServerWorld?"server":"client"));
+
+        for(var listener : this.updateListeners) listener.accept(this);
     }
 }
