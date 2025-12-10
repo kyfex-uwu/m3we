5529866aa6ac11772e6743c78cde30264c812f54
kyfex-uwu <kyfexuwu@gmail.com>
2025-12-10 14:40:42 -0700

    - 404 redirecting

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -1,6 +1,8 @@
 import { PageAttachedRouter, Router } from "arrowjs-aluminum";
 import { html } from "@arrow-js/core";
 import { apiRouter, setLinkGen } from "./api.js";
+if (window.location.pathname.endsWith("404.html"))
+    window.location.href = window.location.href.split("/").slice(0, -1).join("/");
 const router = new PageAttachedRouter(document.getElementById("root"), html `404`);
 setLinkGen(router.link);
 router.addRoute("m3we", new Router()
--- docs/src/index.ts
+++ docs/src/index.ts
@@ -2,6 +2,9 @@ import {PageAttachedRouter, Router} from "arrowjs-aluminum";
 import {html} from "@arrow-js/core";
 import {apiRouter, setLinkGen} from "./api.js";
 
+if(window.location.pathname.endsWith("404.html"))
+    window.location.href = window.location.href.split("/").slice(0,-1).join("/");
+
 const router = new PageAttachedRouter(document.getElementById("root")!, html`404`);
 setLinkGen(router.link);
 
